30d157ad6917a0bcc4143fbafcdd0e38d2bac727
Bennett <bennett.quigley1@gmail.com>
2023-04-21 14:38:08 -0500

    Registration route change

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const apiRouter = require('./api/index');
 const loginController = require('../controllers/logins.controller');
+const userController = require('../controllers/users.controller')
 
 router.use('/api', apiRouter);
 router.post('/login', loginController.postLoginByUserPass);
+router.post('/register', userController.postCreateUser);
 
 module.exports = router;
