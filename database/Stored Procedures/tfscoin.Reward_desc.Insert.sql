f98259490d62a2bdedab97abe5f752c338d9e300
JRyanEv <91679209+JRyanEv@users.noreply.github.com>
2023-04-28 09:18:00 -0500

    Update tfscoin.Reward_desc.Insert.sql

--- database/Stored Procedures/tfscoin.Reward_desc.Insert.sql
+++ database/Stored Procedures/tfscoin.Reward_desc.Insert.sql
@@ -1,16 +1,16 @@
 CREATE DEFINER=`admin`@`%` PROCEDURE `tfscoin.Reward_desc.Insert`(
     IN p_reward_id INT,
     IN p_desc_type VARCHAR(45),
-    IN p_desc_value VARCHAR(45),
-    IN p_inventory INT)
+    IN p_desc_value VARCHAR(45)
+)
 BEGIN
-	-- check if the reward exists for this description
-	DECLARE v_count INT DEFAULT 0;
-    SELECT COUNT(*) INTO v_count FROM `reward` WHERE `reward_id` = p_reward_id;
-    IF v_count = 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid reward_id';
+    -- Check if the reward exists for this description
+    IF EXISTS (SELECT 1 FROM `reward` WHERE `reward_id` = p_reward_id) THEN
+        -- Insert the new description into the reward_desc table
+        INSERT INTO `reward_desc` (`reward_id`, `desc_type`, `desc_value`) 
+        VALUES (p_reward_id, p_desc_type, p_desc_value);
     ELSE
-        INSERT INTO `reward_desc` (`reward_id`, `desc_type`, `desc_value`, `inventory`) 
-	VALUES (p_reward_id, p_desc_type, p_desc_value, p_inventory);
-	END IF;
-END
+        -- Raise an error if the reward_id is invalid
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid reward_id';
+    END IF;
+END;
