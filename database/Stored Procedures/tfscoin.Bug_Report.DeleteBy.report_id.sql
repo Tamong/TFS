0cba366af6036c60ed2b023ddf767b3c6f53ec7c
JRyanEv <91679209+JRyanEv@users.noreply.github.com>
2023-05-05 09:52:25 -0500

    Update tfscoin.Bug_Report.DeleteBy.report_id.sql

--- database/Stored Procedures/tfscoin.Bug_Report.DeleteBy.report_id.sql
+++ database/Stored Procedures/tfscoin.Bug_Report.DeleteBy.report_id.sql
@@ -1,6 +1,6 @@
 CREATE DEFINER=`admin`@`%` PROCEDURE `tfscoin.Bug_Report.DeleteBy.report_id`(
-IN p_report_id INT
+    IN p_report_id INT
 )
 BEGIN
-	DELETE FROM bug_report WHERE report_id = p_report_id;
+    DELETE FROM bug_report WHERE report_id = p_report_id;
 END
