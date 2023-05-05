a755eff6facee3819b967fe1f403665a4a0e7b4e
JRyanEv <91679209+JRyanEv@users.noreply.github.com>
2023-05-05 09:49:00 -0500

    Delete tfscoin.Bug_Bounty.SelectBy.is_active.sql

--- database/Stored Procedures/tfscoin.Bug_Bounty.SelectBy.is_active.sql
+++
@@ -1,6 +0,0 @@
-CREATE DEFINER=`admin`@`%` PROCEDURE `tfscoin.Bug_Bounty.SelectBy.is_active`()
-BEGIN
--- bug bounties are deactivated by setting is_active = 0
--- select active bug bounties by != 0
-SELECT * FROM `bug_bounty` WHERE `is_active` != 0;
-END
