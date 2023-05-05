85f771db1001cd294493582a604cc7cd36ee5e0e
JRyanEv <91679209+JRyanEv@users.noreply.github.com>
2023-05-05 09:47:59 -0500

    Update tfscoin.Employee.SelectBy.username.sql

--- database/Stored Procedures/tfscoin.Employee.SelectBy.username.sql
+++ database/Stored Procedures/tfscoin.Employee.SelectBy.username.sql
@@ -2,7 +2,7 @@ CREATE DEFINER=`admin`@`%` PROCEDURE `tfscoin.Employee.SelectBy.username`(
 IN uname varchar(45)
 )
 BEGIN
-	SELECT *
-    FROM employee
-    WHERE username = uname;
+	SELECT e.ee_ID, e.username, e.is_admin, e.wallet_address, e.private_key 
+    FROM employee e
+    WHERE e.username = uname;
 END
